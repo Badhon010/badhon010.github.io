0fe53c54c93f83e3dd6156cd0fe8fc2ba4f04279
Badhon010 <badhonemail001@gmail.com>
2026-03-13 01:02:26 +0600

    Updated portfolio

--- static/main.js
+++ static/main.js
@@ -29,7 +29,6 @@ function navToSec(id) {
 }
 
 // Update URL while scrolling
-
 const sections = document.querySelectorAll("main section")
 const observer = new IntersectionObserver((entries)=>{
     entries.forEach(entry=>{
@@ -46,6 +45,23 @@ sections.forEach(section=>{
     observer.observe(section)
 })
 
+// About
+function getAge(dateString) {
+    const today = new Date();
+    const birthDate = new Date(dateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDifference = today.getMonth() - birthDate.getMonth();
+    const dayDifference = today.getDate() - birthDate.getDate();
+    if (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
+        age--;
+    }
+    return age;
+}
+
+const ageSpan=document.getElementById('age')
+ageSpan.textContent=getAge('2008-11-18')
+
+
 // Skills
 let skills={
     HTML:['<i class="fab fa-html5" style="color: #e34c26;"></i>',80],
@@ -58,7 +74,6 @@ let skills={
     Git:['<i class="fab fa-git-alt" style="color: #e34c26;"></i>',40]
 }
 let grid=document.getElementById('skills-grid')
-
 for(let skill in skills){
 
     let icon=skills[skill][0]
@@ -66,19 +81,41 @@ for(let skill in skills){
 
     grid.innerHTML+=`
         <div class="skill-card">
-
-            <p class="skill-title">
-                <span class="skill-name">${icon} ${skill}</span>
-                <span class="skill-percent">${percent}%</span>
-            </p>
-
+            <div class="skill-title">
+                <div class="skill-icon">${icon}</div>
+                <div class="skill-details">
+                    <span class="skill-name">${skill}</span>
+                    <span class="skill-percent">${percent}%</span>
+                </div>
+            </div>
             <div class="skill-graph">
                 <div style="width:${percent}%"></div>
             </div>
-
         </div>
     `
 }
+
+// Changing icons
+const icons = {
+    'fab fa-html5':'#e34c26',
+    'fab fa-css3-alt':'#1572b6',
+    'fab fa-js':'#f3d600',
+    'fab fa-python':'#3776ab',
+    'fa-solid fa-code':'#075839',
+    'fab fa-react':'#61dafb',
+    'fas fa-database':'#005a81',
+    'fab fa-git-alt':'#e34c26',
+};
+const icon = document.getElementById('changing-icon');
+const iconClasses = Object.keys(icons);
+let index = 0;
+
+setInterval(() => {
+    icon.className = "fa-bounce";
+    index = (index + 1) % iconClasses.length;
+    icon.classList.add(...iconClasses[index].split(" "));
+    icon.style.color = icons[iconClasses[index]];
+}, 1000);
 
 // Mobile menu
 const menuBtn=document.querySelector("#menu-btn")
@@ -93,3 +130,6 @@ navLinks.forEach((link)=>{
 menuBtn.addEventListener("click",()=>{
     navbar.classList.toggle("active")
 })
+
+// Footer update
+document.getElementById("year").textContent = new Date().getFullYear();
